729578c4b48d8dcdc120eb40dceb311a1a7136a0
atir nayab <nayabatir1@gmail.com>
2022-10-19 02:50:26 +0530

    fix(client): use Audio constructor to play sound

--- client/src/utils/tone/index.ts
+++ client/src/utils/tone/index.ts
@@ -1,5 +1,4 @@
 import store from 'store';
-import * as Tone from 'tone';
 import { FlashMessages } from '../../components/Flash/redux/flash-messages';
 import { Themes } from '../../components/settings/theme';
 
@@ -62,14 +61,13 @@ type ToneStates = keyof typeof toneUrls;
 
 export async function playTone(state: ToneStates): Promise<void> {
   if (toneUrls[state]) {
-    const player = new Tone.Player(toneUrls[state]).toDestination();
+    const audio = new Audio(toneUrls[state]);
 
     const storedVolume = (store.get('soundVolume') as number) ?? 50;
-    const calculateDecibel = -60 * (1 - storedVolume / 100);
 
-    player.volume.value = calculateDecibel;
+    // volume range [0-1], 0 -> muted, 1 -> loudest
+    audio.volume = storedVolume / 100;
 
-    await Tone.loaded();
-    player.start();
+    await audio.play();
   }
 }
